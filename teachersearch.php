60d41db47ab1084e93a49a5cdc6d78b5af27492c
ADSANDY18 <49060791+ADSANDY18@users.noreply.github.com>
2020-03-28 19:44:58 +0530

    Add files via upload

---
+++ teachersearch.php
@@ -0,0 +1,163 @@
+<html>
+    <head>
+        <title>AfterClass: Find tutors now</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <!----------Naviagation--------->
+        <section id="nav-bar">
+        <nav class="navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand" href="index.php"><img src="logoimg.png"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <button type="button" class="btn btn-primary btn-lg active">Primary button</button>
+      </li>
+    </ul>
+  </div>
+</nav>
+        </section>
+        <!---------CityFilter---------->
+        <section id="Cityfilter">
+         <div  class="container">
+         
+         
+        <div class="container">
+      <h3 style="text-align: center; font-weight: bold; margin-top: 20px;">Find your tutor now</h3>
+      <div style="margin-top: 20px;" class="row d-flex aling-content-center justify-content-center">
+        <form class="form-inline" action="#" method="POST">
+                 
+               
+              
+            <div class="form-group">
+              <div class="col-lg-4">
+                    <select name="City" class="form-control form-control-lg">
+                    <option>City</option>
+                    <option value="Chakdaha">Chakdaha</option>
+                    <option value="Palpara">Palpara</option>
+                    <option value="Simurali">Simurali</option>
+                    <option value="Madanpur">Madanpur</option>
+                    <option value="Kalyani">Kalyani</option>
+                  </select>
+                    </div>
+                    </div>
+                 
+
+                    
+                  <div class="form-group">
+                    <div class="col-lg-4">
+                  <select name="Board" class="form-control form-control-lg">
+                    <option>Board</option>
+                    <option value="CBSE">CBSE</option>
+                    <option value="ICSE">ICSE</option>
+                    <option value="WBCSE">WBCSE</option>
+                  </select>
+                    </div>
+                    </div>
+        
+
+                    
+              <div class="form-group">
+                <div class="col-lg-4">
+                  <select name="Class" class="form-control form-control-lg">
+                    <option>Class</option>
+                    <option value="ClassI">Class I</option>
+                    <option value="ClassII">Class II</option>
+                    <option value="ClassIII">Class III</option>
+                    <option value="ClassIV">Class IV</option>
+                    <option value="ClassV">Class V</option>
+                    <option value="ClassVI">Class VI</option>
+                    <option value="ClassVII">Class VII</option>
+                    <option value="ClassVII">Class VIII</option>
+                  </select>
+                    </div>
+                    </div>
+          
+
+                    
+                  <div class="form-group">
+                    <div class="col-lg-4">
+                  <select name="Subject" class="form-control form-control-lg">
+                    <option>Subject</option>
+                    <option value="English">English</option>
+                    <option value="Hindi">Hindi</option>
+                    <option value="Bengali">Bengali</option>
+                    <option value="Mathematics">Mathematics</option>
+                    <option value="Science">Science</option>
+                    <option value="Social Science">Social Science</option>
+                  </select>
+                    </div>
+                    </div>
+          
+                  &nbsp;&nbsp;
+          
+                 <input  type="submit" name="submit" class="btn btn-info btn-lg">
+          
+        </form>
+      </div>
+             
+             
+         </div>
+        </section>
+         
+
+
+        <!-----------AboutandWhy----------->
+        <section id="mainpageabout">
+        <div class="container">
+        <div class="row">
+        <div class="col-md-6">
+        <h2>About AfterClass</h2>
+        <div class="about-content">
+        At AfterClass, you can find experinced and qualified home tutors for all subjects ranging from class I to Class VIII at an affordable price plan. We have teachers for CBSE, ICSE and WBCSE boards capable of teaching in English, Hindi and Bengali language.
+        </div>
+        <button type="button" class="btn btn-primary">Read more>></button>
+        </div>
+        <div class="col-md-6">
+        <h2>Why Home Tutors from AfterClass</h2>
+        <div class="about-content">
+        AfterClass offers variety of study material provided based on the performance of students. Any course plan is made on the experince of student-friendly tutors. Apart from content, there are periodic tests to assess the performance of the student.
+        </div>
+        <button type="button" class="btn btn-primary">Know more>></button>
+        </div>
+        </div>
+        </div>
+        </section>
+        
+        
+    
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
